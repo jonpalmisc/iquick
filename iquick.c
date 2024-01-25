bc7a4537395b4dce431ed24dba25c75e7dba0928
Jon Palmisciano <jp@jonpalmisc.com>
2024-01-25 12:46:45 -0500

    Add restart command

--- iquick.c
+++ iquick.c
@@ -3,11 +3,93 @@
 #include <libimobiledevice/lockdown.h>
 
 #include <stdio.h>
+#include <string.h>
+
+void print_usage(char const *program)
+{
+    printf("Usage: %s <command>\n", program);
+}
+
+typedef enum {
+    COMMAND_NONE,
+    COMMAND_RESTART,
+    COMMAND_ENTER_RECOVERY,
+    COMMAND_EXIT_RECOVERY,
+} command_t;
+
+command_t parse_command(char const *command_str)
+{
+    if (strcmp(command_str, "restart") == 0)
+        return COMMAND_RESTART;
+    if (strcmp(command_str, "recovery") == 0)
+        return COMMAND_ENTER_RECOVERY;
+    if (strcmp(command_str, "unrecovery") == 0)
+        return COMMAND_EXIT_RECOVERY;
+
+    return COMMAND_NONE;
+}
+
+int do_enter_recovery(lockdownd_client_t lockdown)
+{
+    int error = lockdownd_enter_recovery(lockdown);
+    if (error != LOCKDOWN_E_SUCCESS) {
+        fprintf(stderr, "Error: Failed to send device recovery.\n");
+        return 1;
+    }
+
+    return 0;
+}
+
+int do_restart(idevice_t device, lockdownd_client_t lockdown)
+{
+    int result = 0;
+
+    lockdownd_service_descriptor_t service = NULL;
+    diagnostics_relay_client_t diagnostics = NULL;
+
+    int error = lockdownd_start_service(lockdown, "com.apple.mobile.diagnostics_relay", &service);
+    if (error != LOCKDOWN_E_SUCCESS || service == NULL || service->port <= 0) {
+        fprintf(stderr, "Error: Failed to start diagnostics service.\n");
+        result = 1;
+        goto exit;
+    }
+
+    error = diagnostics_relay_client_new(device, service, &diagnostics);
+    if (error != DIAGNOSTICS_RELAY_E_SUCCESS) {
+        fprintf(stderr, "Error: Failed to start diagnostics relay.\n");
+        result = 1;
+        goto exit;
+    }
+
+    error = diagnostics_relay_restart(diagnostics, DIAGNOSTICS_RELAY_ACTION_FLAG_WAIT_FOR_DISCONNECT);
+    if (error == DIAGNOSTICS_RELAY_E_SUCCESS) {
+        printf("Restarting device...\n");
+    } else {
+        fprintf(stderr, "Error: Failed to restart device.\n");
+        result = 1;
+        goto exit;
+    }
+
+exit:
+    if (diagnostics) {
+        diagnostics_relay_goodbye(diagnostics);
+        diagnostics_relay_client_free(diagnostics);
+    }
+
+    if (service)
+        lockdownd_service_descriptor_free(service);
+
+    return result;
+}
 
 int main(int argc, char const **argv)
 {
-    (void)argc;
-    (void)argv;
+    if (argc < 2) {
+        print_usage(argv[0]);
+        return 1;
+    }
+
+    command_t command = parse_command(argv[1]);
 
     idevice_t device = NULL;
     int error = idevice_new(&device, /*udid=*/NULL);
@@ -24,34 +106,23 @@ int main(int argc, char const **argv)
         return 1;
     }
 
-    lockdownd_service_descriptor_t service = NULL;
-    error = lockdownd_start_service(lockdown, "com.apple.mobile.diagnostics_relay", &service);
-    if (error != LOCKDOWN_E_SUCCESS || service == NULL || service->port <= 0) {
-        idevice_free(device);
-        fprintf(stderr, "Error: Failed to start diagnostics service.\n");
-        return 1;
+    int result = 0;
+    switch (command) {
+    case COMMAND_RESTART:
+        result = do_restart(device, lockdown);
+        break;
+    case COMMAND_ENTER_RECOVERY:
+        result = do_enter_recovery(lockdown);
+        break;
+    default:
+        fprintf(stderr, "Unimplemented command.\n");
+        break;
     }
 
-    lockdownd_client_free(lockdown);
-
-    diagnostics_relay_client_t diagnostics = NULL;
-    error = diagnostics_relay_client_new(device, service, &diagnostics);
-    if (error != DIAGNOSTICS_RELAY_E_SUCCESS) {
+    if (lockdown)
+        lockdownd_client_free(lockdown);
+    if (device)
         idevice_free(device);
-        fprintf(stderr, "Error: Failed to start diagnostics relay.\n");
-        return 1;
-    }
 
-    error = diagnostics_relay_restart(diagnostics, DIAGNOSTICS_RELAY_ACTION_FLAG_WAIT_FOR_DISCONNECT);
-    if (error == DIAGNOSTICS_RELAY_E_SUCCESS) {
-        printf("Restarting device...\n");
-    } else {
-        idevice_free(device);
-        fprintf(stderr, "Error: Failed to restart device.\n");
-        return 1;
-    }
-
-    idevice_free(device);
-
-    return 0;
+    return result;
 }
